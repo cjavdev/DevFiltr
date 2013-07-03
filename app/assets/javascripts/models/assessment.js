0bb5c68e1a4fe5ad6c97be7f5c814b97e4839673
CJ Avilla <cjavilla@gmail.com>
2013-07-02 17:02:00 -0700

    removing grader gem

--- app/assets/javascripts/models/assessment.js
+++ app/assets/javascripts/models/assessment.js
@@ -1,13 +1,17 @@
 DevFiltr.Models.Assessment = Backbone.Model.extend({
 	parse: function (data) {
-		var assessment_attempts = new DevFiltr.Collections.AssessmentAttempts({
-			url: this.url() + "/assessment_attempts"
-		});
-		assessment_attempts.set(data.assessment_attempts);
+		if(!this.assessment_attempts) {
+			this.assessment_attempts = new DevFiltr.Collections.AssessmentAttempts({
+				assessment: this
+			});
+		}
+		this.assessment_attempts.set(data.assessment_attempts);
 		delete data.assessment_attempts;
-		var newData = data;
-		newData.assessment_attempts = assessment_attempts;
-		console.log(newData);
-		return newData;
+		
+		return data;
 	},
+	
+	toJSON: function() {
+		var json = Backbone.Model.prototype.toJSON.call(this);
+	}
 });
